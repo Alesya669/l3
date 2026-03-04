1a2fe72d338aed2f32ccfda0e3efc9a259c463d2
Alesya669 <rudenko.alesya2006@mail.ru>
2026-03-04 21:55:15 +0300

    Delete test.go

--- test.go
+++
@@ -1,8 +0,0 @@
-package main
-import "fmt"
-func main() {
-        fmt.Println("Status: 200 Ok")
-        fmt.Println("Content-Type: text/html\n")
-        fmt.Println("<h1>GO CGI is working!</h1>")
-    }
-
